1fbc8ed785d8b17422f8ac7e5a00dc033778d8a4
tiagolrcwb <ti_lr@hotmail.com>
2017-11-04 22:07:18 -0200

    04/11

--- AvMobile/Controllers/FiliaisController.cs
+++ AvMobile/Controllers/FiliaisController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AvMobile.Contexts;
+using AvMobile.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,31 @@ namespace AvMobile.Controllers
 {
     public class FiliaisController : Controller
     {
+        private EFContext context = new EFContext();
         // GET: Filiais
         public ActionResult Index()
         {
+            return View(context.Tbl_Filial.OrderBy(f => f.nome));
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.cidadeId = new SelectList(context.TblCidade.OrderBy(c => c.nome), "id", "nome");
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Filial filial)
+        {
+            try
+            {
+                context.Tbl_Filial.Add(filial);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(filial);
+            }
         }
     }
 }
